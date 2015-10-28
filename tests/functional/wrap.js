a642278272178e54e753628ad184c9d512925ea1
Andrew Chavez <adchavez@gmail.com>
2015-10-28 14:30:18 -0500

    Take screenshots when running tests

--- tests/functional/wrap.js
+++ tests/functional/wrap.js
@@ -1,7 +1,8 @@
 define(function(require) {
 
   var registerSuite = require('intern!object'),
-    assert = require('intern/chai!assert');
+      assert = require('intern/chai!assert'),
+      fs = require('intern/dojo/node!fs');
 
   registerSuite({
 
@@ -9,29 +10,39 @@ define(function(require) {
 
     'first modal shows': function() {
       return this.remote
-        .get(require.toUrl('tests/support/premiumtest/index.html'))
+        .get(require.toUrl('tests/support/premiumtest/1.html'))
         .setFindTimeout(5000)
         .findById('pq-passage-quota-welcome')
           .isDisplayed()
         .then(function(visible) {
           assert.isTrue(visible, 'The welcome modal should show on page load');
-        });
+        })
+        .takeScreenshot()
+        .then(function (data) {
+          fs.writeFileSync('tests/screenshots/test-1.png', data, 'base64');
+        })
+        .end();
     },
 
     'first modal goes away': function() {
       return this.remote
-        .get(require.toUrl('tests/support/premiumtest/index.html'))
+        .get(require.toUrl('tests/support/premiumtest/1.html'))
         .setFindTimeout(5000)
         .findById('pq-welcome-continue')
           .click()
           .end()
-        .findById('pq-welcome-continue')
+        .findById('pq-passage-quota-welcome')
           .setFindTimeout(5000)
           .isDisplayed()
         .then(function(visible) {
           assert.isFalse(visible,
             'The welcome modal should go away when the button is clicked.');
-        });
+        })
+        .takeScreenshot()
+        .then(function (data) {
+          fs.writeFileSync('tests/screenshots/test-2.png', data, 'base64');
+        })
+        .end();
     }
 
   });
