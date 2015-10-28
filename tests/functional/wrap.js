09495240f4cb05b24401e7506904e0c0bed38617
Andrew Chavez <adchavez@gmail.com>
2015-10-28 14:08:23 -0500

    Tests for first page modal

--- tests/functional/wrap.js
+++ tests/functional/wrap.js
@@ -1,20 +1,36 @@
 define(function(require) {
 
   var registerSuite = require('intern!object'),
-      assert = require('intern/chai!assert');
+    assert = require('intern/chai!assert');
 
   registerSuite({
 
-    name: 'wrap',
+    name: 'access meter',
 
-    'login form': function() {
+    'first modal shows': function() {
       return this.remote
-        .get(require.toUrl('tests/support/index.html'))
+        .get(require.toUrl('tests/support/premiumtest/index.html'))
         .setFindTimeout(5000)
-        .findByCssSelector('.text-muted')
-        .getVisibleText()
-        .then(function(text) {
-          assert.strictEqual(text, 'Project name', 'Failure message');
+        .findById('pq-passage-quota-welcome')
+          .isDisplayed()
+        .then(function(visible) {
+          assert.isTrue(visible, 'The welcome modal should show on page load');
+        });
+    },
+
+    'first modal goes away': function() {
+      return this.remote
+        .get(require.toUrl('tests/support/premiumtest/index.html'))
+        .setFindTimeout(5000)
+        .findById('pq-welcome-continue')
+          .click()
+          .end()
+        .findById('pq-welcome-continue')
+          .setFindTimeout(5000)
+          .isDisplayed()
+        .then(function(visible) {
+          assert.isFalse(visible,
+            'The welcome modal should go away when the button is clicked.');
         });
     }
 
