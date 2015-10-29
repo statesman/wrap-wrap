e8db3dd12f3d33b35c3b8554cf8b393534fcf7e2
Andrew Chavez <adchavez@gmail.com>
2015-10-29 09:44:01 -0500

    Test that roadblock persists past 6th page

--- tests/functional/roadblock-modal.js
+++ tests/functional/roadblock-modal.js
@@ -37,6 +37,24 @@ define([
             fs.writeFileSync('tests/screenshots/test-5.png', data);
           })
           .end();
+      },
+
+      'shows on 7th page': function() {
+        return page
+          .doPageviews(6)
+          .getPage()
+          .setFindTimeout(5000)
+          .findById('pq-passage-quota-block')
+            .isDisplayed()
+          .then(function(visible) {
+            assert.isTrue(visible,
+              'The roadblock modal should show on the 6th premium pageview');
+          })
+          .takeScreenshot()
+          .then(function (data) {
+            fs.writeFileSync('tests/screenshots/test-5.png', data);
+          })
+          .end();
       }
 
     };
